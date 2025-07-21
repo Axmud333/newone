f7c739ebeecfcb8ccd58bbf4ccd046804f9dfab1
Axmud333 <ahmad.2005khdir@gmail.com>
2025-07-21 15:36:48 +0300

    Update qdrant_builder.py

--- qdrant_builder.py
+++ qdrant_builder.py
@@ -1,8 +1,40 @@
-from qdrant_builder import qdrant_builder
+from sentence_transformers import SentenceTransformer
+from qdrant_client import QdrantClient, models
+from qdrant_client.http.models import PointStruct
+from qdrant_client.http.models import VectorParams, Distance
+import uuid
 
-datasets = [
-    {"questions": "Who is the Dean of engineering department?", "command": "select dean from colleges where name = 'College of engineering';", "lang": "en"},
-    {"questions": "کێ ڕاگری کۆلێژی ئەندازیاریە؟", "command": "select dean from colleges where name = 'College of engineering';", "lang": "ku"},
-]
+model = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
 
-qdrant_builder(datasets)
+qdrant_client = QdrantClient(url="http://localhost:6333")
+
+my_collection = "uos"
+
+def qdrant_builder(data: list[dict]) -> None:
+    if my_collection not in [col.name for col in qdrant_client.get_collections().collections]:
+        qdrant_client.recreate_collection(
+            collection_name=my_collection,
+            vectors_config=models.VectorParams(size=768, distance=models.Distance.COSINE),
+        )
+
+    for item in data:
+        question= item.get("questions", "").strip()
+        command= item.get("command", "").strip()
+        lang = item.get("lang", "").strip()
+
+        if not question or not command:
+            continue
+        
+        # Yohohohoho!!! Here is the fanci trick!!
+        qdrant_id = str(uuid.uuid4())
+
+        qdrant_client.upsert(
+            collection_name=my_collection,
+            points=[
+                models.PointStruct(
+                    id=qdrant_id,
+                    vector=model.encode(question).tolist(),
+                    payload={"command": command,"lang": lang, "questions": question}
+                )
+            ]
+        )
